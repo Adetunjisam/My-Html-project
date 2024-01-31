ad56edf766ac511f969f8ecdc39ccf6e18f2a609
Adetunjisam <149710443+Adetunjisam@users.noreply.github.com>
2024-01-31 13:53:44 +0100

    Add files via upload

---
+++ school.java
@@ -0,0 +1,18 @@
+pulic Class Name{
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("What is your name?");
+        String Name = input.next();
+
+        System.out.printnt("where do you live?");
+        String Address = input.nextString();
+
+        System.out.println("Where will you like to live in future?");
+        String Job = input.nextString();
+        
+        System.Out.println("My name is "+ Name +("."));
+        System.Out.Println("I live at " + Address + ("."));
+        System.Out.Println("would love to live at "+ "in future. ");
+
+    }
+}
